a685c829dbd07491ecea2f550d9147afe9f533e1
westonperkins <westonperkins5@gmail.com>
2021-06-27 14:49:14 -0400

    test

--- controllers/feed.js
+++ controllers/feed.js
@@ -107,12 +107,11 @@ router.post('/register', async (req, res) => {
 
 
 
-
 // APP FUNCTIONALITY STUFF
 
 
 // get route with conditional to check if the post has 5 flags
-router.get('/feed', (req, res) => {
+router.get('/', (req, res) => {
     Media.find({}).sort({updatedAt:-1})
         .then((posts) => {
             posts.forEach((post) => {
@@ -122,7 +121,7 @@ router.get('/feed', (req, res) => {
                             console.log(media)
                             console.log('deleted')
                         })
-                        .then(res.redirect('/feed'))
+                        .then(res.redirect('/'))
                 }
             })    
             res.render('home.ejs', { posts: posts })
@@ -157,7 +156,7 @@ router.post('/create2', upload.single('imageUpload'),  (req, res, next) => {
             console.log(media)
             console.log("ew")
         })
-        .then(res.redirect('/feed'))
+        .then(res.redirect('/'))
         .then(console.log(req.body))
     } else {
         console.log('no')
@@ -182,7 +181,7 @@ router.post('/create3', upload.single('imageUpload'),  (req, res, next) => {
         Media.create(product)
         .then((media) => {
             console.log(media + " mediua")
-            res.redirect('/feed')
+            res.redirect('/')
             // undefined coming from line 84 ^
         })
     } 
@@ -204,7 +203,7 @@ router.post('/create', upload.single('imageUpload'),  (req, res, next) => {
         .then((media) => {
             console.log(media)
         })
-        .then(res.redirect('/feed'))
+        .then(res.redirect('/'))
         .then(console.log(req.body.text + " REQ"))
         .catch((err) => {
             console.log(err)
@@ -224,14 +223,14 @@ router.put('/:id', (req, res, next) => {
     Media.findOneAndUpdate({_id:id}, { $inc: {flag: 1}})
     .then((media) => {
         console.log(media + " flags")
-        res.redirect('/feed')
+        res.redirect('/')
     })
     } 
     else if(req.body.likes == 'on') {
     Media.findOneAndUpdate({_id:id}, { $inc: {likes: 1}})
     .then((media) => {
         console.log(media + " likes")
-        res.redirect('/feed')
+        res.redirect('/')
     })
     } 
     else if(req.body.url != '' && req.body.text != '') {
@@ -242,14 +241,14 @@ router.put('/:id', (req, res, next) => {
     .then((media) => {
         console.log(media)
     })
-    .then(res.redirect('/feed'))
+    .then(res.redirect('/'))
     console.log('put')
     } 
     else if(req.body.caption != '') {
     Media.updateOne({_id: id}, { $set: { caption: req.body.caption }}, {new:true})
     .then((media) => {
         console.log(media + " caption")
-        res.redirect('/feed')
+        res.redirect('/')
     })
     }
     else {
@@ -264,7 +263,7 @@ router.delete('/:id', (req, res) => {
         console.log(media)
         console.log('delete')
     })
-    .then(res.redirect('/feed'))
+    .then(res.redirect('/'))
 })
 
 
